e8ec7fc887034b5a433a886991115241f825d406
JeevanReddy0 <94883102+JeevanReddy0@users.noreply.github.com>
2022-03-23 10:01:01 +0000

    p8

--- p8final.c
+++ p8final.c
@@ -1,1 +1,19 @@
 #include<stdio.h>
+typedef struct _triangle
+{
+float base,altitude,area;
+} Triangle;
+int input_n()
+{
+  int n;
+  printf("enter the number of triangles:\n");
+  scanf("%d",&n);
+  return n;
+}
+Triangle input_triangle()
+{
+  Triangle a;
+  printf("enter the base and altitude of triangle\n");
+  scanf("%f%f",&a.base,&a.altitude);
+  return
+}
